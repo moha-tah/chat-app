2c63c00aa485ff1401d80033b7177f159927233f
Mohamed Tahiri <moha.tah@icloud.com>
2025-05-12 12:02:30 +0200

    ✨ refactor: update API endpoints and enhance web interface
    - Changed API endpoint paths in ChatController, InvitationController, and UserController to remove the "/api" prefix for a cleaner URL structure.
    - Enhanced WebController by adding a UserService for user management and updated methods to retrieve user data.
    - Introduced new HTML templates for home, users, and update-user pages to improve the user interface and experience.

--- backend/src/main/java/com/sr03/chat_app/controllers/ChatController.java
+++ backend/src/main/java/com/sr03/chat_app/controllers/ChatController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/chats")
+@RequestMapping("chats")
 public class ChatController {
 
     @Autowired
--- backend/src/main/java/com/sr03/chat_app/controllers/WebController.java
+++ backend/src/main/java/com/sr03/chat_app/controllers/WebController.java
@@ -1,34 +1,41 @@
 package com.sr03.chat_app.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.sr03.chat_app.services.UserService;
+
 @Controller
 @RequestMapping("/admin")
 public class WebController {
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping()
-    public String adminPage() {
-        return "admin";
+    public String homePage() {
+        return "home";
     }
 
     @GetMapping("/users")
-    public String usersPage() {
+    public String usersPage(Model model) {
+        model.addAttribute("users", userService.getAllUsers());
         return "users";
     }
 
     @GetMapping("users/{id}")
     public String userPage(@PathVariable int id, Model model) {
-        model.addAttribute("id", id);
+        model.addAttribute("user", userService.getUserById(id));
         return "user";
     }
 
     @GetMapping("users/{id}/update")
     public String updateUserPage(@PathVariable int id, Model model) {
-        model.addAttribute("id", id);
+        model.addAttribute("user", userService.getUserById(id));
         return "update-user";
     }
 }
