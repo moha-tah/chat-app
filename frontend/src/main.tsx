ecf1bf0c038e6aca5a08048cb00e321bc16f3fcd
Mohamed Tahiri <moha.tah@icloud.com>
2025-06-02 13:12:43 +0200

    ✨ feat: refactor application structure by removing App component and CSS
    - Deleted App.tsx and App.css files to streamline the application structure.
    - Integrated routing and page components directly in main.tsx, enhancing navigation and organization.
    - Added routes for Home, Login, Sign Up, and Chats pages with corresponding titles.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,56 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+import ChatsPage from "./pages/ChatsPage";
+import { Toaster } from "./components/ui/sonner";
+import PageTitle from "./components/PageTitle";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Toaster />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <PageTitle title="Accueil" />
+              <Home />
+            </>
+          }
+        />
+        <Route
+          path="/chats"
+          element={
+            <>
+              <PageTitle title="Mes chats" />
+              <ChatsPage />
+            </>
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <>
+              <PageTitle title="Connexion" />
+              <LoginPage />
+            </>
+          }
+        />
+        <Route
+          path="/sign-up"
+          element={
+            <>
+              <PageTitle title="Inscription" />
+              <SignUpPage />
+            </>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
